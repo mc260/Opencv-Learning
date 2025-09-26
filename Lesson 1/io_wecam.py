de942c00bd8a62d751ac543e416a843f56972da6
mc260 <2637936786@qq.com>
2025-09-26 19:31:01 +0800

    Lesson No.1

--- Lesson 1/io_wecam.py
+++ Lesson 1/io_wecam.py
@@ -1,14 +1,14 @@
 import cv2
 import numpy as np
 
-wecam=cv2.VideoCapture(0)
+wecam=cv2.VideoCapture(0)                     #捕获编号为0的网络摄像头
 
-while(True):
+while(True):                                  #由于摄像头的图像实时更新 故永远循环播放
     ret,frame=wecam.read()
     cv2.imshow('frame',frame)
     if cv2.waitKey(40) & 0xFF == ord('q'):
         break
 
-
+#释放内存
 wecam.release()
 cv2.destroyAllWindows()
