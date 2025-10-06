d237e295f91015ee729fd754890481b43f800d2d
mc260 <2637936786@qq.com>
2025-10-07 00:31:32 +0800

    Lesson 5

--- Lesson 5/adaptive_threshold.py
+++ Lesson 5/adaptive_threshold.py
@@ -1,4 +1,4 @@
-#全局阈值案例
+#自适应阈值案例
 import cv2
 import numpy as np
 
@@ -8,8 +8,8 @@ img_copy=cv2.resize(img,(800,640))          #太大了裁一下
 
 img_gray=cv2.cvtColor(img_copy,cv2.COLOR_BGR2GRAY)
 
-ret,thresh = cv2.threshold(img_gray,85 ,255,cv2.THRESH_BINARY)
-cv2.GaussianBlur(thresh,(11,11),3)
+thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,30)
+#cv2.GaussianBlur(thresh,(11,11),3)
 
 #cv2.imshow('original', img_copy)
 cv2.imshow('gray', img_gray)
