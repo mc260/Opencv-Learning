f0e2b8fc2a47ab9d21a192fee2f895ec1c284542
mc260 <2637936786@qq.com>
2025-10-07 12:03:13 +0800

    Lesson 5

--- Lesson 5/Threshold.py
+++ Lesson 5/Threshold.py
@@ -3,13 +3,13 @@ import cv2
 import numpy as np
 
 img=cv2.imread('text.jpg')
-img_copy=cv2.resize(img,(800,640))          #太大了裁一下
+img_copy=cv2.resize(img,(800,640))                                          #太大了裁一下
 
 
-img_gray=cv2.cvtColor(img_copy,cv2.COLOR_BGR2GRAY)
+img_gray=cv2.cvtColor(img_copy,cv2.COLOR_BGR2GRAY)                                #BGR转灰度图
 
-ret,thresh = cv2.threshold(img_gray,85 ,255,cv2.THRESH_BINARY)
-cv2.GaussianBlur(thresh,(11,11),3)
+ret,thresh = cv2.threshold(img_gray,85 ,255,cv2.THRESH_BINARY)      #全局阈值应用
+cv2.GaussianBlur(thresh,(11,11),3)                                   #高斯模糊清噪点
 
 #cv2.imshow('original', img_copy)
 cv2.imshow('gray', img_gray)
--- Lesson 5/adaptive_threshold.py
+++ Lesson 5/adaptive_threshold.py
@@ -3,13 +3,13 @@ import cv2
 import numpy as np
 
 img=cv2.imread('text.jpg')
-img_copy=cv2.resize(img,(800,640))          #太大了裁一下
+img_copy=cv2.resize(img,(800,640))                                                                                         #太大了裁一下
 
 
-img_gray=cv2.cvtColor(img_copy,cv2.COLOR_BGR2GRAY)
+img_gray=cv2.cvtColor(img_copy,cv2.COLOR_BGR2GRAY)                                                                               #转化灰度图
 
-thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,21,30)
-#cv2.GaussianBlur(thresh,(11,11),3)
+thresh = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,31,18)         #自适应阈值
+cv2.GaussianBlur(thresh,(11,11),3)                                                                                  #高斯模糊降噪
 
 #cv2.imshow('original', img_copy)
 cv2.imshow('gray', img_gray)
