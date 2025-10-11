838cc27b0c76ce1fb30490f6e952e0c80bbf1f9c
mc260 <2637936786@qq.com>
2025-10-12 00:58:26 +0800

    Week 3 Last

--- Lesson 6/Edge detection.py
+++ Lesson 6/Edge detection.py
@@ -4,9 +4,17 @@ import numpy as np
 img=cv2.imread('brid.png')
 img=cv2.resize(img,(800,600))
 
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+
 img_edge = cv2.Canny(img,100,200)           #canny api 实现边缘绘制
+img_dli=cv2.dilate(img_edge,kernel)
+img_erode=cv2.erode(img_edge,kernel)
+
 
 cv2.imshow('image',img)
 cv2.imshow('edge',img_edge)
+cv2.imshow('dli',img_dli)
+cv2.imshow('erode',img_erode)
+
 
 cv2.waitKey(0)
